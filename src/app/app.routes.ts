152385a20c5c8b06111c2876f8c97c58dc481b94
cfchape97 <chasechape@gmail.com>
2024-08-28 00:11:43 -0400

    setting up tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent},
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route to redirect to radio
+    { path: '**', redirectTo: '/login' } // Redirect to radio for any other invalid routes
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
